e88bc55aa361975d1d03d3c816ecab843c50a1b2
戴强 <daiq@fxiaoke.com>
2019-03-29 18:12:03 +0800

    404

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const { httpAccess } = require('./lib/logger');
+const {
+	httpAccess
+} = require('./lib/logger');
 const router = require('./src/router');
 const errors = require('./src/errors');
 
@@ -9,8 +12,16 @@ const app = express();
 app.set('env', process.env.NODE_ENV);
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+// app.engine('html', function (filePath, options, callback) { // define the template engine
+// 	fs.readFile(filePath, function (err, content) {
+// 		if (err) return callback(err)
+// 		// this is an extremely simple template engine
+// 		var rendered = content.toString().replace('#title#', '<title>' + options.title + '</title>');
+// 		return callback(null,  content.toString());
+// 	})
+// })
+// app.set('views', path.join(__dirname, './src/views'));
+// app.set('view engine', 'html');
 
 app.use(httpAccess());
 app.use(express.json());
@@ -36,20 +47,20 @@ app.get('/', (req, res) => {
 app.use(errors);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-	// next(createError(404));
-	next(res.send(404));
-});
+// app.use(function (req, res, next) {
+// 	// next(createError(404));
+// 	next(res.send(404));
+// });
 
-// error handler
-app.use((err, req, res, next) => {
-	// set locals, only providing error in development
-	res.locals.message = err.message;
-	res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use((err, req, res, next) => {
+// 	// set locals, only providing error in development
+// 	res.locals.message = err.message;
+// 	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-	// render the error page
-	res.status(err.status || 500);
-	res.render('error');
-});
+// 	// render the error page
+// 	res.status(err.status || 500);
+// 	res.render('error');
+// });
 
 module.exports = app;
--- src/errors.js
+++ src/errors.js
@@ -1,21 +1,40 @@
 /**
  * @description 错误处理控制器
  */
-const {logger} = require('./di');
+const path = require('path');
+const {
+	logger,
+} = require('./di');
 
 // 404
 function noFound(req, res, next) {
-	const err = new Error('Not Found');
-	err.status = 404;
-	next(err);
+	// res.render('404');
+	res.sendFile(path.join(__dirname, './views/404.html'));
 };
 
 // others
+// function error(err, req, res, next) {
+// 	if (err && err.status !== 404) {
+// 		logger.getLogger('system').error(err);
+// 	}
+// 	res.status(500).end();
+// }
+// error handler
 function error(err, req, res, next) {
-	if (err && err.status !== 404) {
-		logger.getLogger('system').error(err);
-	}
-	res.status(500).end();
-}
+	console.log(err);
+	if (err && err.status === 404) {
+				res.end();
+				return;
+			}
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// render the error page
+	res.status(err.status || 500);
+	// res.render('error');
+	res.end();
+};
+
 
 module.exports = [noFound, error];
